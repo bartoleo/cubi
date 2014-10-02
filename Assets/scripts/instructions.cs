b74aa27489c1e1c9f119e78e805614f6deeeba1f
Leo Bartoloni <bartololeo74@gmail.com>
2014-10-02 14:16:52 +0200

    added instruction text

--- Assets/scripts/instructions.cs
+++ Assets/scripts/instructions.cs
@@ -27,16 +27,20 @@ public class instructions : MonoBehaviour {
 		
 		subTitleStyle = new GUIStyle(GUI.skin.label);
 		subTitleStyle.fontSize = 21;
-		subTitleStyle.alignment = TextAnchor.UpperLeft;
+		subTitleStyle.alignment = TextAnchor.MiddleCenter;
 
 		textStyle = new GUIStyle(GUI.skin.label);
 		textStyle.fontSize = 15;
 		textStyle.alignment = TextAnchor.MiddleCenter;
 
 		GUI.Label(new Rect(Screen.width/2-200, 30, 400, 40), "CUBI", titleStyle);
-		GUI.Label(new Rect(Screen.width/2-200, 70, 400, 40), "by Bartoleo", subTitleStyle);
-		
-		GUI.Label(new Rect(Screen.width/2-200, 120, 400, 230), "Instructions... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ... blah blah ", subTitleStyle);
+		GUI.Label(new Rect(Screen.width/2-200, 70, 400, 40), "Instructions", subTitleStyle);
+
+		if (gamestate.Instance.isGame ()) {
+			GUI.Label (new Rect (Screen.width / 2 - 200, 120, 400, 230), "Match two cubes by color to destroy them.\n\nYou can rotate & pinch.\n\nEnjoy!", textStyle);
+		} else {
+			GUI.Label (new Rect (Screen.width / 2 - 200, 120, 400, 230), "Select cubes by touch.\n\nDestroy selected cubes with 'Destroy Selected'\n\nYou can rotate & pinch.\n\nEnjoy!", textStyle);
+		}
 
 		if(GUI.Button(new Rect (Screen.width/2-90, 350, 180, 60), "Back to Menu",buttonStyle))
 		{
